57eefeaf0b74b54b00d75d2bff8124c3d1d4e1df
Leedehai <leedehai@outlook.com>
2019-08-19 17:39:36 -0700

    best efforts to handle ()-initialization in ctor init list

--- camel2snake.py
+++ camel2snake.py
@@ -5,8 +5,9 @@
 #
 #
 # Limitations caused by lack of syntax awareness:
-#  a. it leaves variable initialized by ()-style (e.g. in constructor) untouched if the
-#     variable does not end with an underscore, as it thinks the variable is a function:
+#  a. despite best efforts (see besteffort_* below), in corner cases it leaves some
+#     variables initialized by ()-style expression untouched if the variable does
+#     not end with an underscore, as it thinks the variable is a function:
 #       what it does: variableName(initArgument) => variableName(init_argument)
 #       it should do: variableName(initArgument) => variable_name(init_argument)
 #  b. it replaces function name if it is used as a pointer, as it thinks the function
@@ -23,7 +24,10 @@ import argparse
 
 REGEX_PIECE_1 = r"(\A|(?<=\W))([a-jl-z]|[a-z]{2,})([A-Z][a-z]*|[0-9]+)+_?(?=[^\w\(]|$)"
 REGEX_PIECE_2 = r"(\A|(?<=\W))([a-jl-z]|[a-z]{2,})([A-Z][a-z]*|[0-9]+)+_(?=\()"
+REGEX_PIECE_IN_CTOR_INIT = r"(\A|(?<=\)\s:\s|\S\),\s)|(?<=\A:\s|\s\s))[a-z]+([A-Z][a-z]*|[0-9]+)+(?=\()"
 DROMEDARY_CAMEL_CASE_VAR = re.compile(REGEX_PIECE_1 + "|" + REGEX_PIECE_2)
+DROMEDARY_CAMEL_CASE_VAR_IN_CTOR_INIT = re.compile(
+    REGEX_PIECE_1 + "|" + REGEX_PIECE_2 + "|" + REGEX_PIECE_IN_CTOR_INIT)
 
 BOOLEAN_PREFIXES = [
     "is", "are", "was", "were",
@@ -31,20 +35,21 @@ BOOLEAN_PREFIXES = [
     "does", "do", "did", "done",
     "find", "found", "get", "got"
 ]
-COMMON_ABBREVIATIONS = [ # NOTE no "num", "it", "src", "dest", "std", "ret"
-    ("obj", "object"),  ("msg", "message"),  ("res", "result"), ("buf", "buffer"),
-    ("vec", "vector"),  ("seq", "sequence"), ("cnt", "count"),  ("mem",  "memory"),
-    ("ans", "answer"),  ("loc", "location"), ("elem", "element")
+COMMON_ABBREVIATIONS = [
+    # NOTE no "obj", "num", "it", "iter", "var", "src", "dest", "std",
+    #         "ret", "init", "ptr", "op"
+    ("res", "result"),  ("buf", "buffer"),  ("vec", "vector"),  ("msg", "message"),  
+    ("seq", "sequence"), ("cnt", "count"),  ("mem", "memory"),  ("val", "value"),  
+    ("loc", "location"), ("ans", "answer"), ("ctx", "context"), ("elem", "element"),
+    ("ty", "type"),
 ]
 
-def compute_snake_case(camel_case, filepath="", testing=False):
-    splitted_words = re.findall(r"[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|[0-9]|_|$)|[0-9]+|\_", camel_case)
+CAMEL_CASE_PIECE_REGEX = re.compile(r"[A-Z]?[a-z]+|[A-Z]+(?=[A-Z]|[0-9]|_|$)|[0-9]+|\_")
+def compute_snake_case(camel_case, testing=False):
+    splitted_words = CAMEL_CASE_PIECE_REGEX.findall(camel_case)
     if testing:
-        print("'%s'\n\t=> %s" % (camel_case, splitted_words))
-    if len(splitted_words) < 2:
-        raise RuntimeError("len(splitted_words) < 2: '%s' => %s, in file %s" % (camel_case, splitted_words, filepath))
-    if '_' in splitted_words[:-1]:
-        raise RuntimeError("'_' inside '%s'[:-1], in file %s" % (camel_case, filepath))
+        print("%-15s => %s" % (camel_case, splitted_words))
+    assert len(splitted_words) >= 2
     splitted_words = list(map(lambda w : w.lower(), splitted_words))
     # special rules in conversion
     ends_with_underscore = False
@@ -70,39 +75,65 @@ def compute_snake_case(camel_case, filepath="", testing=False):
     return snake_case
 
 MAX_LOOP_STEPS = 16 # unlikely to have more than this number of camelCase variables in one line
-def process_one_line(old_line, filepath, testing=False):
+def process_one_line(old_line, in_ctor_init_list=False, testing=False):
     step_count, instance_count = 0, 0
     line = old_line
     while True:
         step_count += 1
         if step_count > MAX_LOOP_STEPS:
-            raise RuntimeError("maximum loop steps (%d) exceeded, line: '%s'" % (
+            raise RuntimeError("maximum loop steps (%d) exceeded, line:\n%s" % (
                 MAX_LOOP_STEPS, old_line))
-        matchObj = re.search(DROMEDARY_CAMEL_CASE_VAR, line)
+        regex_obj = DROMEDARY_CAMEL_CASE_VAR
+        if in_ctor_init_list:
+            regex_obj = DROMEDARY_CAMEL_CASE_VAR_IN_CTOR_INIT
+        matchObj = re.search(regex_obj, line)
         if not matchObj:
             return line, instance_count
         camel_case_var = matchObj.group(0)
         camel_case_var_start, camel_case_var_end = matchObj.start(), matchObj.end()
         instance_count += 1
-        snake_case_var = compute_snake_case(camel_case_var, filepath, testing)
+        snake_case_var = compute_snake_case(camel_case_var, testing)
         line = line[:camel_case_var_start] + snake_case_var + line[camel_case_var_end:]
 
 def process_one_file(filepath, echo, rewrite):
     with open(filepath, 'r') as f:
         raw_lines = f.readlines()
     instance_count, new_lines = 0, []
-    for raw_line in raw_lines:
+    if echo:
+        print() # newline
+    # best effort of determing whether "foo(a)" is a ()-style initialization or a function call
+    besteffort_in_ctor_init_list = False
+    for i, raw_line in enumerate(raw_lines):
         old_line = raw_line.rstrip()
+        ### check if in ctor initializer list (1)
+        besteffort_index_of_colon = old_line.find(':') # -1 if absent
+        if besteffort_index_of_colon >= 0:
+            if ((besteffort_index_of_colon >= 2
+                 and old_line[besteffort_index_of_colon - 2:besteffort_index_of_colon] == ") "
+                 and " ?" not in old_line[:besteffort_index_of_colon])
+                or (i >= 1 and len(old_line[:besteffort_index_of_colon].strip()) == 0
+                 and raw_lines[i - 1].rstrip().endswith(')')
+                 and (" ?" not in raw_lines[i - 1]))):
+                besteffort_in_ctor_init_list = True
+        ### essential works
+        besteffort_ctor_list_flag = '#' if besteffort_in_ctor_init_list else ' '
         if echo:
-            print("\x1b[38;5;217m" + old_line + "\x1b[0m")
-        new_line, instance_count_in_line = process_one_line(old_line, filepath)
+            print("-" + besteffort_ctor_list_flag + "|\x1b[38;5;217m" + old_line + "\x1b[0m")
+        new_line, instance_count_in_line = process_one_line(old_line, besteffort_in_ctor_init_list)
         if echo:
-            print("\x1b[32m" + new_line + "\x1b[0m")
+            print("+" + besteffort_ctor_list_flag + "|\x1b[32m" + new_line + "\x1b[0m")
         new_lines.append(new_line)
         instance_count += instance_count_in_line
-    if rewrite:
-        with open(filepath, 'r') as f:
+        ### check if in ctor initializer list (2)
+        if besteffort_in_ctor_init_list:
+            besteffort_index_of_open_brace = old_line.find('{') # -1 if absent
+            if besteffort_index_of_open_brace >= 0:
+                besteffort_in_ctor_init_list = False
+    if rewrite and instance_count:
+        with open(filepath, 'w') as f:
             f.write('\n'.join(new_lines) + '\n')
+    if not rewrite and not echo:
+        print('\n'.join(new_lines) + '\n')
     return instance_count
 
 def is_c_cxx(filename):
@@ -113,11 +144,13 @@ def is_c_cxx(filename):
 
 def work(args):
     if args.test != None:
-        new_line, _ = process_one_line(args.test, filepath=None, testing=True)
+        new_line, _ = process_one_line(args.test, testing=True)
         print("\x1b[32;m" + new_line + "\x1b[0m")
         return
-    files_to_read = [] if os.path.isdir(args.path) else [ args.path ]
-    if len(files_to_read) == 0:
+    files_to_read = []
+    if os.path.isfile(args.path):
+        files_to_read = [ args.path ]
+    else: # os.path.isdir(args.path)
         for (dirpath, dirnames, filenames) in os.walk(args.path, followlinks=True):
             for filename in filter(lambda f : is_c_cxx(f), filenames):
                 if (((os.sep + "test-inputs") in dirpath)
@@ -126,16 +159,18 @@ def work(args):
                     continue
                 files_to_read.append(os.path.join(dirpath, filename))
     for filepath in files_to_read:
-        sys.stdout.write("%s .." % (filepath))
-        sys.stdout.flush()
+        sys.stderr.write("%s .." % (filepath))
+        sys.stderr.flush()
         processed_instances = process_one_file(filepath, echo=args.echo, rewrite=args.rewrite)
-        sys.stdout.write("\r%s count: %d\n" % (filepath, processed_instances))
-        sys.stdout.flush()
+        sys.stderr.write("\r%s count: %d\n" % (filepath, processed_instances))
+        sys.stderr.flush()
+    if not args.rewrite:
+        sys.stderr.write("\nTo rewrite files, use '--rewrite'; to echo lines, use '--echo'\n")
     return 0
 
 def main():
     parser = argparse.ArgumentParser(description="C/C++ vairable name camelCase => snake_case")
-    parser.add_argument("-p", "--path", type=str, default=None,
+    parser.add_argument("path", nargs='?', default=None,
                         help="file or directory path")
     parser.add_argument("--rewrite", action="store_true",
                         help="rewrite visited files")
@@ -147,16 +182,16 @@ def main():
     has_error = False
     if args.test == None and args.path == None:
         has_error = True
-        print("[Error] you need to use either '--path' or '--test'")
+        sys.stderr.write("[Error] you need to give argument 'path' or use option '--test'\n")
     if args.test != None and (args.path != None or args.rewrite or args.echo):
         has_error = True
-        print("[Error] '--test' can only be used alone")
-    if (args.rewrite or args.echo) and args.path == None:
+        sys.stderr.write("[Error] '--test \"..\"' can only be used alone, but you gave something else too\n")
+    if args.test == None and (args.rewrite or args.echo) and args.path == None:
         has_error = True
-        print("[Error] specify path using '--path'")
+        sys.stderr.write("[Error] you need to give the path argument while using '--rewrite' or '--echo'\n")
     if args.path != None and not os.path.exists(args.path):
         has_error = True
-        print("[Error] not found: %s" % args.path)
+        sys.stderr.write("[Error] not found: %s\n" % args.path)
     return 1 if has_error else work(args)
 
 if __name__ == "__main__":
